b009c004f5cace067e57dba51a909a6ac9816064
Matheus Bessa <76802830+mdabessa@users.noreply.github.com>
2021-03-24 21:32:53 -0300

    Update utils.py

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -14,32 +14,33 @@ def nround(num, n=3):
         return float(num)
 
 
-
 def fdate(par, date):
-    num = ''
-    c = 0
+    def extract(par):
+        times = []
+        l = 0
+        num = ''
+        word = ''
+        for i in par:
+            try:
+                _num = str(int(i))
+                if l == 1:
+                    times.append([num, word])
+                    l = 0
+                    num = _num
+                    word = ''
+                else:
+                    num += _num
+
+            except:
+                l = 1
+                word += i
+
+        times.append([num, word])
+        return times
+
 
     param = par.replace(' ','')
-    for i in param:
-        try:
-            num += str(int(i))
-            c += 1
-        except:
-            break
-    
-    if c == 0:
-        return
-
-    if num == 0:
-        return
-
-    try:
-        num = int(num)
-        s = param[c:]
-        s = s.lower()
-    except:
-        return
-
+    times = extract(param)
 
     leg = {
         'minute':['m', 'min', 'minute', 'minutes', 'minuto', 'minutos'],
@@ -49,21 +50,29 @@ def fdate(par, date):
         'year':['y', 'year', 'years', 'a', 'ano', 'anos']
     }
 
+    t = 0
+    for time in times:
+        if time[0] == '' or time[1] == '':
+            continue
+        
+        num = int(time[0])
+        word = time[1]
 
-    if s in leg['minute']:
-        t = num*60
-    elif s in leg['hour']:
-        t = num*3600
-    elif s in leg['day']:
-        t = num*86400
-    elif s in leg['mounth']:
-        t = num*2592000
-    elif s in leg['year']:
-        t = num*31536000
-    else:
-        return None
+        if word in leg['minute']:
+            t += num*60
+        elif word in leg['hour']:
+            t += num*3600
+        elif word in leg['day']:
+            t += num*86400
+        elif word in leg['mounth']:
+            t += num*2592000
+        elif word in leg['year']:
+            t += num*31536000
+        else:
+            return None
+
 
     delta = datetime.timedelta(seconds=t)
 
     limit = 3153600000 #100 years
-    return None if t > limit else date + delta
+    return None if (t > limit or t==0) else date + delta
